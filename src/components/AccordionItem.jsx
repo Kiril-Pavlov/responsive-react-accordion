5aef8782ba6e7afcc00a09e0bef9bb0788da83ff
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-02 15:18:07 +0100

    addeed some styling for the transition and some icons

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 
+import {BsChevronUp,BsChevronDown} from "react-icons/bs"
+
 const AccordionItem = ({question, answer, active}) => {
 const [isActive,setIsActive] = useState(active)
   return (
     <div>
         <div className='flex flex-row py-2 px-6 items-center justify-between bg-slate-500 font-bold text-slate-100'>
             <div>{question}</div>
-            <div onClick={()=>setIsActive(!isActive)}>{(isActive === false ? "Open" : "Close")}</div>
+            <div onClick={()=>setIsActive(!isActive)}>{(isActive === false ? <BsChevronDown /> : <BsChevronUp />)}</div>
         </div>
-        <div className={(isActive===false ? "hidden" : 'flex py-2 px-6 border-2')}>{answer}</div>
+        <div className={(isActive===false ? "h-0 opacity-0 transition-all duration-500" : 'flex h-auto opacity-100 py-2 px-6 border-2 transition-all duration-500')}>{answer}</div>
     </div>
   )
 }
