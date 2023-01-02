977d3d5628c0e22adc72ef1509deeaa258fe9543
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-02 15:22:07 +0100

    addeed description and styles to it

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Description = () => {
   return (
-    <div>Description</div>
+    <div className='text-xl w-10/12 md:w-8/12 lg:6/12 border-x-4 rounded-3xl p-6 mx-auto text-center'>
+        Responsive react accordion. Multiple can be opened at the same time.
+    </div>
   )
 }
 
