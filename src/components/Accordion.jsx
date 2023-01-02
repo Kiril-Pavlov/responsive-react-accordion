a1ce196a5d5a4b3021ffc70172d949266e0212a0
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-02 14:39:51 +0100

    added some fake data

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
 const Accordion = () => {
+
+  const accordionData = [
+    {
+      id:1,
+      question:"What is your name?",
+      answer: "Kiril"
+    },
+    {
+      id:2,
+      question:"What is your surname?",
+      answer: "Pavlov"
+    },
+    {
+      id:3,
+      question:"Write something about yourself?",
+      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum, maiores placeat vel voluptatibus laboriosam quo suscipit earum veniam iure labore, cupiditate molestiae quisquam alias in unde optio. Quos veritatis facere pariatur adipisci corporis nihil sint harum fuga mollitia amet?"
+    },
+  ]
+
+  
+
   return (
     <div>Accordion</div>
   )
