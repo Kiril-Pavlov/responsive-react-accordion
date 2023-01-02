86aebe538b8f928ff630913497b67db487b18d7e
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-02 15:06:02 +0100

    accordion logic done

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -8,27 +8,31 @@ const Accordion = () => {
     {
       id:1,
       question:"What is your name?",
-      answer: "Kiril"
+      answer: "Kiril",
+      active: false,
     },
     {
       id:2,
       question:"What is your surname?",
-      answer: "Pavlov"
+      answer: "Pavlov",
+      active: false,
     },
     {
       id:3,
       question:"Write something about yourself?",
-      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum, maiores placeat vel voluptatibus laboriosam quo suscipit earum veniam iure labore, cupiditate molestiae quisquam alias in unde optio. Quos veritatis facere pariatur adipisci corporis nihil sint harum fuga mollitia amet?"
+      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum, maiores placeat vel voluptatibus laboriosam quo suscipit earum veniam iure labore, cupiditate molestiae quisquam alias in unde optio. Quos veritatis facere pariatur adipisci corporis nihil sint harum fuga mollitia amet?",
+      active: false,
+
     },
   ]
 
   
 
   return (
-    <div className='flex flex-col w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 my-8 mx-auto'>
+    <div className='flex flex-col gap-2 w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 my-8 mx-auto'>
       {accordionData.map(item =>{
         return (
-          <AccordionItem key={item.id} question={item.question} answer={item.answer}/>
+          <AccordionItem key={item.id} question={item.question} answer={item.answer} active={item.active}/>
         )
       })}
     </div>
--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AccordionItem = ({question, answer}) => {
+const AccordionItem = ({question, answer, active}) => {
+const [isActive,setIsActive] = useState(active)
   return (
     <div>
-        <div>
+        <div className='flex flex-row py-2 px-6 items-center justify-between bg-slate-500 font-bold text-slate-100'>
             <div>{question}</div>
-            <div>Open</div>
+            <div onClick={()=>setIsActive(!isActive)}>{(isActive === false ? "Open" : "Close")}</div>
         </div>
-        <div>{answer}</div>
+        <div className={(isActive===false ? "hidden" : 'flex py-2 px-6 border-2')}>{answer}</div>
     </div>
   )
 }
